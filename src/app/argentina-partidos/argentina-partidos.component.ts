68159ccd26556041e3a4a88e22827f0ed1c5912b
Javier Alonso <javoalon@gmail.com>
2022-11-29 12:10:34 -0300

    Trabajo finalizado.

--- src/app/argentina-partidos/argentina-partidos.component.ts
+++ src/app/argentina-partidos/argentina-partidos.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-argentina-partidos',
-  templateUrl: './argentina-partidos.component.html',
-  styleUrls: ['./argentina-partidos.component.scss']
+	selector: 'app-argentina-partidos',
+	templateUrl: './argentina-partidos.component.html',
+	styleUrls: ['./argentina-partidos.component.scss']
 })
 export class ArgentinaPartidosComponent implements OnInit {
-
-  constructor() { }
+	matches = [{
+		'match':'Argentina vs Arabia Saudita',
+		'day':'22/11',
+		'hour':'7:00',
+		'place':'Estadio Lusail, Lusail, Qatar'
+	},{
+		'match':'Argentina vs Mexico',
+		'day':'26/11',
+		'hour':'16:00',
+		'place':'Estadio Lusail, Lusail, Qatar'
+	},{
+		'match':'Polonia vs Argentina',
+		'day':'30/11',
+		'hour':'16:00',
+		'place':'Estadio 974, Doha, Qatar'
+	}]
+	constructor() { }
 
   ngOnInit(): void {
-  }
+		}
 
-}
+	}
